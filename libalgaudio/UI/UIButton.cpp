dfa1d3d0d65da60f4780af3f9301a561e21ad4fc
Rafał Cieślak <rafalcieslak256@ubuntu.com>
2015-07-08 13:42:21 +0200

    Full button area

--- libalgaudio/UI/UIButton.cpp
+++ libalgaudio/UI/UIButton.cpp
@@ -7,7 +7,12 @@ UIButton::UIButton(std::string t) : text(t){
 
 void UIButton::Draw(DrawContext& c){
   c.SetColor(50,50,50);
-  c.DrawLine(0,0,100,100);
+  c.DrawLine(0,0,c.width,0);
+  c.DrawLine(0,0,0,c.height);
+  c.DrawLine(0,0,c.width,c.height);
+  c.DrawLine(0,c.height,c.width,0);
+  c.DrawLine(c.width,0,c.width,c.height);
+  c.DrawLine(0,c.height,c.width,c.height);
 }
 
 } // namespace AlgAudio
