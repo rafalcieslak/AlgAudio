523bac506dc28a7583bf9467abbf155cabaef996
Rafał Cieślak <rafalcieslak256@ubuntu.com>
2015-07-16 13:36:45 +0200

    Created windows packaging script

---
+++ package/package-windows.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#Run this package inside the build dir once everything has been compiled
+
+DIR=package-windows
+
+mkdir -f $DIR
+cp algaudio.exe libalgaudio.dll liblo-7.dll $DIR
+cp -r fonts sc $DIR
+mkdir $DIR/modules
+cp modules/*.xml $DIR/modules/.
+cp modules/*.aa.dll $DIR/modules/.
+
+# Copy all necessary dlls
+ldd $DIR/*.dll | grep mingw | cut -d ' ' -f 3 | xargs -i cp {} $DIR/.
+# Repeat few times for transitive dependencies
+ldd $DIR/*.dll | grep mingw | cut -d ' ' -f 3 | xargs -i cp {} $DIR/.
+ldd $DIR/*.dll | grep mingw | cut -d ' ' -f 3 | xargs -i cp {} $DIR/.
+ldd $DIR/*.dll | grep mingw | cut -d ' ' -f 3 | xargs -i cp {} $DIR/.
