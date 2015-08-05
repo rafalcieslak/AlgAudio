4ad613bbc29982537c7488d6e7f8fd7784d3f20c
Rafał Cieślak <rafalcieslak256@ubuntu.com>
2015-08-05 14:56:00 +0200

    Thicker lines for wires

--- libalgaudio/SDLFix/SDLFix.cpp
+++ libalgaudio/SDLFix/SDLFix.cpp
@@ -79,8 +79,10 @@ void SDLFix::RenderSetLineSmoothing(SDL_Renderer* renderer, bool enabled){
   GL_RenderData *data = (GL_RenderData*) renderer->driverdata;
   if(enabled){
     data->glEnable(GL_LINE_SMOOTH);
+    data->glLineWidth(2.0);
   }else{
     data->glDisable(GL_LINE_SMOOTH);
+    data->glLineWidth(1.0);
   }
 }
 
