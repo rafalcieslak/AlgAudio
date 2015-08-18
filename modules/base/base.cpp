3486a2f454ec3d582347ed58a475f82d7e535809
Rafał Cieślak <rafalcieslak256@ubuntu.com>
2015-08-18 11:00:01 +0200

    MIDI note input module

--- modules/base/base.cpp
+++ modules/base/base.cpp
@@ -33,7 +33,38 @@ public:
       // Set the param controler.
       GetParamControllerByID( "ctrl" + std::to_string(m.number) )->Set(m.value);
     });
-    std::cout << "AAAAA INSTANCE" << std::endl;
+  }
+};
+
+class MIDINote : public AlgAudio::Module{
+public:
+  std::shared_ptr<AlgAudio::ParamController> gate, note, velocity;
+  int notecount;
+  void on_init(){
+
+    note     = GetParamControllerByID("note");
+    velocity = GetParamControllerByID("velocity");
+    gate     = GetParamControllerByID("gate");
+
+    subscriptions += AlgAudio::SCLang::on_midi_message_received.Subscribe([this](AlgAudio::MidiMessage m){
+      if(m.type == AlgAudio::MidiMessage::Type::NoteOn){
+        note->Set( mtof(m.number) );
+        velocity->Set( m.velocity );
+        notecount++;
+        if(notecount == 1){ // first note
+          gate->Set(0.0);
+          gate->Set(1.0);
+        }
+      }else if(m.type == AlgAudio::MidiMessage::Type::NoteOff){
+        notecount--;
+        if(notecount == 0){ // last note
+          gate->Set(0.0);
+        }
+      }
+    });
+  }
+  float mtof(float m) const{
+    return 440.0f * exp2((m - 69.0f)/12.0f);
   }
 };
 
@@ -43,6 +74,7 @@ void delete_instance(void* obj){
 }
 void* create_instance(const char* name){
   if(strcmp(name,"MIDICtrl")==0) return new MIDICtrl();
+  if(strcmp(name,"MIDINote")==0) return new MIDINote();
   return nullptr;
 }
 
