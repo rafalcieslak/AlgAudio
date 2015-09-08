fced3a73cb7433a787a449e32756bcea5492aabf
Rafał Cieślak <rafalcieslak256@ubuntu.com>
2015-09-08 13:29:58 +0200

    Packaging fixes. Simple SDK generation target. #6

--- package/package-windows.sh
+++ package/package-windows.sh
@@ -6,10 +6,12 @@ DIR=package-windows
 
 mkdir -p $DIR
 cp algaudio.exe libalgaudio.dll liblo-7.dll $DIR
-cp -r fonts sc $DIR
-mkdir $DIR/modules
+cp -r fonts $DIR
+mkdir -p $DIR/modules
+mkdir -p $DIR/sc
 cp modules/*.xml $DIR/modules/.
 cp modules/*.aa.dll $DIR/modules/.
+cp sc/*.sc $DIR/sc/.
 cp -r ../examples $DIR 
 
 # Copy all necessary dlls
