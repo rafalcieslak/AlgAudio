dcaad9cb682354b69a93b5e7c5401ba2a5a12729
Rafał Cieślak <rafalcieslak256@ubuntu.com>
2015-09-25 11:19:56 +0200

    Created packaging targets for linux

---
+++ package/package-linux.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Run this package inside the build dir once everything has been compiled
+
+DIR=package
+
+mkdir -p $DIR
+cp algaudio.bin libalgaudio.so $DIR
+cp -r fonts $DIR
+mkdir -p $DIR/modules
+mkdir -p $DIR/sc
+cp modules/*.xml $DIR/modules/.
+cp modules/*.aa.so $DIR/modules/.
+cp sc/*.sc $DIR/sc/.
+cp -r ../examples $DIR 
